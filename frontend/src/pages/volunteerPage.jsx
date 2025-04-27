9705282fd8e98d0aab346791a1353e4c0735f397
zoiebonnette03 <bonnettezoie@gmail.com>
2025-04-27 12:26:06 -0500

    me vs css

--- frontend/src/pages/volunteerPage.jsx
+++ frontend/src/pages/volunteerPage.jsx
@@ -26,52 +26,63 @@ export default function VolunteerPage() {
   }, [user]);
 
   return (
-    <div className="page-container">
-      {user ? (
-        <>
-          <h1>
-            Welcome, {user.first_name} {user.last_name}!
-          </h1>
-        </>
-      ) : (
-        <p>Loading user info...</p>
-      )}
-
-      {shelter ? (
-        <>
-          <h3>{shelter.shelter_name}</h3>
-          <p>
-            {shelter.addr_street}, {shelter.addr_city}, {shelter.addr_state}
-          </p>
-        </>
-      ) : (
-        <p>Shelter info...</p>
-      )}
-
-      <h3>Your Shifts</h3>
-      {shift && shift.length > 0 ? (
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Day</th>
-              <th>Time</th>
-            </tr>
-          </thead>
-          <tbody>
-            {shift.map((s, index) => (
-              <tr key={index}>
-                <td>{s.shift_day}</td>
-                <td>
-                  {s.shift_start_time.slice(0, 5)} -{" "}
-                  {s.shift_end_time.slice(0, 5)}
-                </td>
+    <div className="volunteer-page">
+      <div className="top-bar">
+        <div className="welcome">
+          {user ? (
+            <>
+              <h1 className="welcome">
+                Welcome, {user.first_name} {user.last_name}!
+              </h1>
+            </>
+          ) : (
+            <p>Loading user info...</p>
+          )}
+        </div>
+        <div className="shelter-info">
+          {shelter ? (
+            <>
+              <h3>{shelter.shelter_name}</h3>
+              <p>
+                {shelter.addr_street}, {shelter.addr_city}, {shelter.addr_state}
+              </p>
+              <p>{`(${shelter.phone.slice(0, 3)}) ${shelter.phone.slice(
+                3,
+                6
+              )}-${shelter.phone.slice(6)}`}</p>
+            </>
+          ) : (
+            <p>Shelter info...</p>
+          )}
+        </div>
+      </div>
+      <hr />
+      <div className="shift-info">
+        <h3>Shifts</h3>
+        {shift && shift.length > 0 ? (
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Day</th>
+                <th>Time</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      ) : (
-        <p>No shifts found.</p>
-      )}
+            </thead>
+            <tbody>
+              {shift.map((s, index) => (
+                <tr key={index}>
+                  <td>{s.shift_day}</td>
+                  <td>
+                    {s.shift_start_time.slice(0, 5)} -{" "}
+                    {s.shift_end_time.slice(0, 5)}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        ) : (
+          <p>No shifts found.</p>
+        )}
+      </div>
     </div>
   );
 }
