8ef82407ac86cce8f451756e2215f83be346137e
zoiebonnette03 <bonnettezoie@gmail.com>
2025-04-29 18:38:12 -0500

    opush

--- backend/api/volunteer.js
+++ backend/api/volunteer.js
@@ -59,6 +59,52 @@ const volunteerQueries = {
       );
     });
   },
+  getReqResources: (volunteer_id) => {
+    return new Promise((resolve, reject) => {
+      db.query(
+        `SELECT r.resource_name, r.resource_quantity, rq.quantity FROM RESOURCES r
+        JOIN REQUIRES rq ON r.resource_id = rq.resource_id
+        JOIN SERVICE s ON s.service_type = rq.service_type
+        AND s.service_type =
+        (
+            SELECT service_type FROM VOLUNTEER vr
+            WHERE vr.volunteer_id = ?
+        ) 
+        JOIN SHELTER sh ON sh.shelter_id = r.shelter_id
+        AND sh.shelter_id =
+        (
+            SELECT shelter_id FROM VOLUNTEER vr
+            WHERE vr.volunteer_id = ?
+        )`,
+        [volunteer_id, volunteer_id],
+        (err, results) => {
+          if (err) {
+            console.error("Failed to load resources:", err);
+            reject(err);
+          } else {
+            resolve(results);
+          }
+        }
+      );
+    });
+  },
+  getService: (volunteer_id) => {
+    return new Promise((resolve, reject) => {
+      db.query(
+        `SELECT service_type FROM VOLUNTEER
+        WHERE volunteer_id = ?`,
+        [volunteer_id],
+        (err, results) => {
+          if (err) {
+            console.error("Failed to load skills:", err);
+            reject(err);
+          } else {
+            resolve(results);
+          }
+        }
+      );
+    });
+  },
 };
 
 module.exports = volunteerQueries;
